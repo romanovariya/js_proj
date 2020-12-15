4f2271aa691e70d263a4de91e6fb7c83ba64cefe
Riya Romanova <riyaromanova13@gmail.com>
2020-12-15 18:06:51 +0300

    feat(script.js): timer on site

---
+++ script/script.js
@@ -0,0 +1,54 @@
+window.addEventListener('DOMContentLoaded', function () { 
+    'use strict';
+
+
+    //timer
+    function countTimer(deadline) { 
+        let timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() { 
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+            
+            if (timeRemaining <= 0) {
+                hours = '00';
+                minutes = '00';
+                seconds = '00';
+            }
+            if(seconds < 10) {
+                seconds = `0${seconds}`;
+            }
+            if (hours < 10) {
+                hours = `0${hours}`;
+            }
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+            
+            return {timeRemaining, hours, minutes, seconds};
+        }
+
+        function updateclock() { 
+            let timer = getTimeRemaining(),
+                timerId;
+            
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+            if(timer.timeRemaining > 0) {
+                timerId = setInterval(updateclock, 1000);
+            } else {
+                clearInterval(timerId);
+            }
+        }
+        updateclock();
+    }
+
+    countTimer('16 december 2020');
+ });
