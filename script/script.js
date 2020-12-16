a627da6e5795732fc2327e119698fed7563f60a6
Riya Romanova <riyaromanova13@gmail.com>
2020-12-16 17:45:35 +0300

    feat(script.js): add menu animation & popup animation

--- script/script.js
+++ script/script.js
@@ -53,5 +53,77 @@ window.addEventListener('DOMContentLoaded', function () {
         updateclock();
     }
 
-    countTimer('16 december 2020');
- });
+    countTimer('30 december 2020');
+
+    //меню
+
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+
+    //popup
+ 
+    const togglePopup = () => {
+
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+        popupContent.style.position = 'absolute';
+        popupBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (screen.width > 768) {
+                    popupContent.animate([
+                    { left: '-38%' }, 
+                    { left: '38%' }
+                ], {
+                    duration: 2000,
+                    easing: 'ease-in-out',
+                    iterations: 1
+                });
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            if(screen.width >= 768) {
+                popupContent.animate([
+                    { left: '38%' }, 
+                    { left: '-38%' }
+                ], {
+                    duration: 2000,
+                    easing: 'ease-in-out',
+                    iterations: 1
+                });
+            setTimeout(() => {
+                popup.style.display = 'none';
+            }, 2000);
+            } else {
+                popup.style.display = 'none';
+            }
+        });
+
+    };
+
+    togglePopup();
+
+});
+
+ 
